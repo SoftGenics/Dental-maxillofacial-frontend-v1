2b76947a6ec8f5a8f54e856adb6c302dc26a7db9
Apoorv Kumar <96113133+SoftGenics@users.noreply.github.com>
2022-03-13 11:40:01 +0530

    Add files via upload

---
+++ precache-manifest.f5f41e541631672ee1d2d82fe3adb07e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d2fe196b4b5cf1d32c6",
+    "url": "/The-Dental-Maxillofacial-clinic/static/css/main.9b8ddb09.chunk.css"
+  },
+  {
+    "revision": "0d2fe196b4b5cf1d32c6",
+    "url": "/The-Dental-Maxillofacial-clinic/static/js/main.4fa14649.chunk.js"
+  },
+  {
+    "revision": "dc70cd2613c353945dd2",
+    "url": "/The-Dental-Maxillofacial-clinic/static/js/runtime~main.80cbb663.js"
+  },
+  {
+    "revision": "19a2950b340fd8cbe5b8",
+    "url": "/The-Dental-Maxillofacial-clinic/static/css/2.5360ee0b.chunk.css"
+  },
+  {
+    "revision": "19a2950b340fd8cbe5b8",
+    "url": "/The-Dental-Maxillofacial-clinic/static/js/2.f8515da3.chunk.js"
+  },
+  {
+    "revision": "cb04a4fcc274232b68da9b80ed083b88",
+    "url": "/The-Dental-Maxillofacial-clinic/static/media/arrow-down-mint.cb04a4fc.svg"
+  },
+  {
+    "revision": "76b887575c5740ccdbba1b826547cf0d",
+    "url": "/The-Dental-Maxillofacial-clinic/index.html"
+  }
+];
